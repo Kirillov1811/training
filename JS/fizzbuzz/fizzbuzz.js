b4e268178ee1e3d2fbaee2fe5c918b2b30f32152
евгений кириллов <evgenijkirillov@evgenijs-MacBook-Air.local>
2022-06-28 16:08:19 +0300

    add array solvation

--- JS/fizzbuzz/fizzbuzz.js
+++ JS/fizzbuzz/fizzbuzz.js
@@ -1,35 +1,17 @@
-let y = 0;
-
-
 let arr = [];
-//for (let i = 1; i <= 100; i++){
-  /*  arr [i]= i;
-   // console.log(arr[i]);
-
-  if ((i % 3 === 0) && (i % 5 === 0)){
-    arr[i] = 'FIZZBUZZ';
-  }
-  //else {
+for (let i = 1; i <= 100; i++){
+  arr [i]= i;
+  if ((i % 3 === 0) && (i % 5 === 0)){arr[i] = 'FIZZBUZZ ' + i;}
+  else {
   
-   if (i % 3 === 0){
-    arr[i] = 'FIZZ';
-    
-    //console.log('FIZZ');
-   }
-   if (i % 5 === 0){
-    arr[i] = 'BUZZ';
-
-  // }
-  }
+   if (i % 3 === 0){ arr[i] = 'FIZZ ' + i;}
+   if (i % 5 === 0){arr[i] = 'BUZZ ' + i;}
+ }
+ console.log(arr[i]); 
+}
 
 
-
-
-   console.log(arr[i]); */
-
-
-
-   for (let i = 1; i <= 100; i++){
+   /*for (let i = 1; i <= 100; i++){
 
     if ((i % 3 === 0) && (i % 5 === 0)){
         console.log('fizzbuzz');
@@ -53,6 +35,4 @@ let arr = [];
  //  if ((i % 3 === 0) && (i % 5 === 0)){
  //       console.log('FIZZBUZZ');
 
- //  }
-   
-}
+  }*/
