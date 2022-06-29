afd85ef66aaf85c93430f65b8a2f9ad127ab182b
евгений кириллов <evgenijkirillov@evgenijs-MacBook-Air.local>
2022-06-29 15:51:08 +0300

    solve triangle task

--- JS/fizzbuzz/fizzbuzz.js
+++ JS/fizzbuzz/fizzbuzz.js
@@ -22,6 +22,7 @@ for (let i = 1; i <= 100; i++){
     console.log('fizz');
    }
 
+   
    else if (i % 5 === 0){
 
     console.log('buzz');
---
+++ JS/triangle/triangle.js
@@ -0,0 +1,8 @@
+let a = "#"; 
+console.log(a);
+for (i = 0; i < 6; i++){
+   a = a + '#';
+    console.log(a);
+}
+
+
